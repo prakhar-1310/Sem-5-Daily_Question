0d470ead9ab396792be5b0ab58cbdac9dea250b8
prakhar-1310 <164357278+prakhar-1310@users.noreply.github.com>
2025-08-04 23:09:36 +0530

    Time: 7 ms (96.86%), Space: 55.4 MB (40.84%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int totalFruit(int[] arr) {
+        int start=0;
+        int end =0;
+        int type1=-1;
+        int count1=0;
+        int type2 =-1;
+        int count2=0;
+        int ans=0;
+        int type=0;
+        while (end<arr.length){
+            //grow
+            if(arr[end]==type1 || type1==-1){
+                type1=arr[end];
+                count1++;
+            }
+            else if(arr[end]==type2 || type2==-1){
+                type2=arr[end];
+                count2++;
+            }
+
+            // shrink
+            else{
+                while (count1>0 && count2>0){
+                    if (arr[start]==type1){
+                        count1--;
+                    }
+                    else if(arr[start]==type2){
+                        count2--;
+                    }
+                    start++;
+                }
+                if (count1==0){
+                    type1=arr[end];
+                    count1++;
+                }
+                else if (count2==0){
+                    type2=arr[end];
+                    count2++;
+                }
+            }
+            ans = Math.max(ans,end-start+1);
+            end++;
+        }
+        
+        return ans;
+    }
+}
