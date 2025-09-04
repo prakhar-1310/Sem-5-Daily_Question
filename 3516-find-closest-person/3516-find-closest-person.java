dc78b5a79f2c13948aa605c6d37100e7e7bfd413
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-09-05 01:26:11 +0530

    Time: 0 ms (100%), Space: 40.9 MB (55.36%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a = Math.abs(x-z);
+        int b = Math.abs(y-z);
+        if(a==b){
+            return 0;
+        }
+        else if (a>b){
+            return 2;
+        }
+        return 1;
+    }
+}
