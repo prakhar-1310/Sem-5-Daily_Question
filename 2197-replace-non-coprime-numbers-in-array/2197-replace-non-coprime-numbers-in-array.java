7ab98ceb5e9e9d1fa18ffeafbadd88436e0f91f1
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-09-18 22:57:52 +0530

    Time: 39 ms (31.58%), Space: 58.1 MB (92.81%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.java
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.java
@@ -1,26 +1,36 @@
 class Solution {
     public List<Integer> replaceNonCoprimes(int[] nums) {
-        List<Integer> stack = new ArrayList<>();
-        
-        for (int num : nums) {
-            while (!stack.isEmpty()) {
-                int top = stack.get(stack.size() - 1);
-                int g = gcd(top, num);
-                if (g == 1) {
-                    break;
-                }
-                // merge top with current num (via LCM)
-                stack.remove(stack.size() - 1);
-                num = (top / g) * num;  // safer LCM
-            }
-            stack.add(num);
+        List<Integer>list = new ArrayList<>();
+        if(nums.length==1){
+            list.add(nums[0]);
+            return list;
         }
         
-        return stack;
+        for(int i : nums){
+            list.add(i);
+            while(list.size()>1){
+                int a = list.get(list.size()-1);
+                int b = list.get(list.size()-2);
+                int g = gcd(a,b);
+                if(g>1){
+                    list.remove(list.size()-1);
+                    list.remove(list.size()-1);
+                    long lcm = (1L*a*b)/g;
+                    list.add((int)lcm);
+                }
+                if(g==1){
+                    break;
+                }
+            }
+        }
+        return list;
     }
     
     private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+        if(b==0){
+            return a;
+        }
+        int g = gcd(b, a%b);
+        return g;
     }
 }
