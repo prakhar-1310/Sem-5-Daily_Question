925a5c27dedb19686853572bce0c2fabd84d2a00
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-10-04 15:30:01 +0530

    Time: 31 ms (14.48%), Space: 45.9 MB (52.92%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.java
+++ 0838-push-dominoes/0838-push-dominoes.java
@@ -1,28 +1,28 @@
 class Solution {
     public String pushDominoes(String dominoes) {
-        int right[] = new int [dominoes.length()];//left to right
-        int left[] = new int [dominoes.length()]; //right to left
+        int nearestRightPush[] = new int [dominoes.length()];//left to right
+        int nearestLeftPush[] = new int [dominoes.length()]; //right to left
 
         for(int i=0;i<dominoes.length();i++){
             char ch =dominoes.charAt(i);
             if(ch=='L'){
-                right[i]=-1;
+                nearestRightPush[i]=-1;
             }
             else{
                 if(i==0){
                     if(ch=='R'){
-                        right[i]=i;
+                        nearestRightPush[i]=i;
                     }
                     else{
-                        right[i]=-1;
+                        nearestRightPush[i]=-1;
                     }   
                 }
                 else{
                     if(ch=='R'){
-                        right[i]=i;
+                        nearestRightPush[i]=i;
                     }
                     else{
-                        right[i]=right[i-1];
+                        nearestRightPush[i]=nearestRightPush[i-1];
                     }
                 }
             }
@@ -31,23 +31,23 @@ class Solution {
         for(int i=dominoes.length()-1;i>=0;i--){
             char ch =dominoes.charAt(i);
             if(ch=='R'){
-                left[i]=-1;
+                nearestLeftPush[i]=-1;
             }
             else{
                 if(i==dominoes.length()-1){
                     if(ch=='L'){
-                        left[i]=i;
+                        nearestLeftPush[i]=i;
                     }
                     else{
-                        left[i]=-1;
+                        nearestLeftPush[i]=-1;
                     }   
                 }
                 else{
                     if(ch=='L'){
-                        left[i]=i;
+                        nearestLeftPush[i]=i;
                     }
                     else{
-                        left[i]=left[i+1];
+                        nearestLeftPush[i]=nearestLeftPush[i+1];
                     }
                 }
             }
@@ -55,18 +55,18 @@ class Solution {
 
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<dominoes.length();i++){
-            if(right[i]==-1 && left[i]==-1){ // means no effect
+            if(nearestRightPush[i]==-1 && nearestLeftPush[i]==-1){ // means no effect
                 sb.append('.'); 
             }
-            else if(right[i]==-1){ // no effect of right push
+            else if(nearestRightPush[i]==-1){ // no effect of right push
                 sb.append('L');
             }
-            else if (left[i]==-1){ // no effect of left push
+            else if (nearestLeftPush[i]==-1){ // no effect of left push
                 sb.append('R');
             }
             else{
-                int distfromleft = Math.abs(i-right[i]);// pushing right
-                int distfromright = Math.abs(i-left[i]);//pushing left
+                int distfromleft = Math.abs(i-nearestRightPush[i]);// pushing right
+                int distfromright = Math.abs(i-nearestLeftPush[i]);//pushing left
 
                 if(distfromleft<distfromright){ // left push distance is more
                     sb.append('R');
