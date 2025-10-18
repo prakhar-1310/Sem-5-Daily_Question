31984aa722116664c0ca113a488614135b198a7a
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-10-18 20:13:48 +0530

    Time: 1 ms (82.69%), Space: 42 MB (51.07%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int numDecodings(String s) {
+        int dp[] = new int [s.length()];
+        Arrays.fill(dp,-1);
+        return helper(s,0,dp);
+    }
+
+    public int helper(String ques, int idx, int dp[]){
+        if(idx>ques.length()){
+            return 0;
+        }
+
+        if(idx==ques.length()){
+            return 1;
+        }
+
+        if(ques.charAt(idx)=='0'){
+            return 0;
+        }
+
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+
+        int ans = helper(ques,idx+1,dp);
+        
+        if(ques.length()>idx+1){
+            int num = Integer.parseInt(ques.substring(idx,idx+2));
+            if(num<=26){
+                ans+=helper(ques,idx+2,dp);
+            }
+        }
+        
+    
+
+        return dp[idx]=ans;
+    }
+}
