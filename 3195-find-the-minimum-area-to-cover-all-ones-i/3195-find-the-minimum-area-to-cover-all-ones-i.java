474763d8ea7e320e6152823ead53a0afc7594f64
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-11-20 14:45:24 +0530

    Time: 5 ms (92.98%), Space: 240.1 MB (11.71%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.java
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,25 +1,26 @@
 class Solution {
     public int minimumArea(int[][] grid) {
-        int minr=Integer.MAX_VALUE;
-        int maxr=Integer.MIN_VALUE;
-        int minc=Integer.MAX_VALUE;
-        int maxc=Integer.MIN_VALUE;
+        int minr = Integer.MAX_VALUE;
+        int maxr = Integer.MIN_VALUE;
+        int minc = Integer.MAX_VALUE;
+        int maxc = Integer.MIN_VALUE;
+
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){   
-                    minr=Math.min(minr,i);
-                    maxr=Math.max(maxr,i);
-                    minc=Math.min(minc,j);
-                    maxc=Math.max(maxc,j);
+                if(grid[i][j]==1){
+                    minr = Math.min(i,minr);
+                    maxr = Math.max(i,maxr);
+                    minc = Math.min(j,minc);
+                    maxc = Math.max(j,maxc);
                 }
-            } 
+            }
         }
-        
-        if (minr == Integer.MAX_VALUE) return 0;
-        
-        int len = maxc-minc+1;
-        int bre = maxr-minr+1;
-        return len*bre;
+        if(minr == Integer.MAX_VALUE){ //  means none of element is 1
+            return 0;
+        }
+        int height = maxr-minr+1;
+        int width = maxc-minc+1;
 
+        return (height*width);
     }
 }
