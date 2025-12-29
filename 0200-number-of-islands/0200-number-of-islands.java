22155d0f62cfa9d6315af2b3d12665358b83e2ad
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-12-29 12:35:55 +0530

    Time: 3 ms (86.68%), Space: 52.4 MB (42.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,22 +4,28 @@ class Solution {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
+                    dfs(i,j,grid);
                     count++;
-                    helper(grid,i,j);
                 }
             }
         }
         return count;
     }
 
-    public void helper(char grid[][], int i, int j){
-        if(i>=grid.length || i<0 || j<0 || j>=grid[0].length || grid[i][j]=='0'){
+    public void dfs(int i, int j, char [][]grid){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0'){
             return;
         }
         grid[i][j]='0';
-        helper(grid, i-1, j); // top
-        helper(grid, i+1, j); // down
-        helper(grid, i, j-1); // left
-        helper(grid, i, j+1); // right
+
+        // top
+        dfs(i-1,j,grid);
+        // bottom
+        dfs(i+1,j,grid);
+        // left
+        dfs(i,j-1,grid);
+        // right
+        dfs(i,j+1,grid);
+
     }
 }
