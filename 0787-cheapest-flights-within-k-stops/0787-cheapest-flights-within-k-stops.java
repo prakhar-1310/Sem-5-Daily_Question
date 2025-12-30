fb8f91441e87165b7d0421187313dd2f0dba8ff1
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-12-30 12:39:08 +0530

    Time: 43 ms (5.09%), Space: 48 MB (5.23%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -13,11 +13,13 @@ class Solution {
 
         // HashSet<Integer>visited = new HashSet<>();
         PriorityQueue<Pair>pq = new PriorityQueue<>((a,b)->{
-            return a.stop-b.stop;
+            return a.cost-b.cost;
         });
         pq.add(new Pair(src,0,0));
 
         int ans = Integer.MAX_VALUE;
+        int[] minStop = new int[n];
+        Arrays.fill(minStop, Integer.MAX_VALUE);
 
         while(!pq.isEmpty()){
             Pair rm = pq.poll();
@@ -28,6 +30,9 @@ class Solution {
             if(rm.stop>k+1){
                 continue;
             }
+
+            if(rm.stop > minStop[rm.vtx]) continue;
+            minStop[rm.vtx] = rm.stop;
 
             if(rm.vtx == dst && k+1>=rm.stop){
                 ans = Math.min(ans,rm.cost);
