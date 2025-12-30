e6096e2449224b3b721ec1b15474dac30c0c9635
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2025-12-30 22:28:24 +0530

    Time: 3 ms (72.67%), Space: 53.7 MB (53.84%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int arr[] = new int[n+1]; // 1 to n 
+        for(int i[] : trust){
+            arr[i[0]]--;
+            arr[i[1]]++;
+        }
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
