c4ac0736b191d8364ea0b51bd0c4ae4688f369f2
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-01-02 14:21:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.java
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.java
@@ -10,41 +10,40 @@ class Solution {
             int e1 = e[0];
             int e2 = e[1];
             int cost = e[2];
-            map.get(e1).put(e2,Math.min(cost,map.get(e1).getOrDefault(e2,Integer.MAX_VALUE)));
-            map.get(e2).put(e1,Math.min(cost,map.get(e2).getOrDefault(e1,Integer.MAX_VALUE)));
+            map.get(e1).put(e2,cost);
+            map.get(e2).put(e1,cost);
         }
 
+        HashSet<Integer>visited = new HashSet<>();
         PriorityQueue<Pair>pq = new PriorityQueue<>((a,b)->{
             return a.cost-b.cost;
         });
         pq.add(new Pair(0,0));
         int dist[] = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
+        Arrays.fill(dist,-1);
         dist[0]=0;
 
         while(!pq.isEmpty()){
             Pair rm = pq.poll();
 
-            if(dist[rm.vtx]<rm.cost) continue;
+            if(visited.contains(rm.vtx)){
+                continue;
+            }
+            visited.add(rm.vtx);
             if(rm.cost>=disappear[rm.vtx]){
                 continue;  
             }
+            dist[rm.vtx] = rm.cost;
 
             for(int ngbr : map.get(rm.vtx).keySet()){
-                int newCost = rm.cost+map.get(rm.vtx).get(ngbr);
-                if(dist[ngbr]>newCost && newCost<disappear[ngbr]){
-                    dist[ngbr] = newCost;
+                if(!visited.contains(ngbr)){
                     pq.add(new Pair(ngbr, rm.cost+map.get(rm.vtx).get(ngbr)));
                 }
             }
         }
 
-        for(int i=0;i<n;i++){
-            if(dist[i]==Integer.MAX_VALUE){
-                dist[i]=-1;
-            }
-        }
         return dist;
+        
     }
 
     class Pair{
