4052124d8fc4ed5393e9d4908fddd042c29a622a
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-01-03 15:46:40 +0530

    Time: 2 ms (99.75%), Space: 46.2 MB (53.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,38 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return lis(nums);
-        
-    }
-    public static int lis(int nums[]){
-        int dp[] = new int [nums.length];
-        Arrays.fill(dp,1);
-        for(int i=0;i<nums.length;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    
-                }
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0];
+        int len=1;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>dp[len-1]){
+                dp[len]=nums[i];
+                len++;
+            }
+            else{
+                int idx = bs(len,dp,nums[i]);
+                dp[idx] = nums[i];
             }
         }
 
-        int ans=0;
-        for(int i : dp){
-            ans = Math.max(ans,i);
+        return len;
+    }
+
+    public int bs(int len, int arr[], int tar){
+        int low =0;
+        int high = len-1;
+        int idx = high;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]>=tar){
+                idx = mid;
+                high=mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        return ans;
+
+        return idx;
     }
 }
