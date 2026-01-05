78bcc2f42a708e4bea7563f969ec72669c9f87ec
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-01-05 14:30:25 +0530

    Time: 0 ms (100%), Space: 42.2 MB (75.56%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -11,4 +11,15 @@ class Solution {
 
         return -1;
     }
+    // k=9 // num
+    // let two terms => x9,y9
+    // x*10+9 + y*10+9 = num
+    // x*10 + y*10 + k*i = num// i is no of elements in a set mean i = no. of k
+    // (num-k*i)%10=x+y
+    // since x and y are integer
+    // therefore (num-k*i)%10==0
+
+    // And 
+    // k*i<=num
+    
 }
