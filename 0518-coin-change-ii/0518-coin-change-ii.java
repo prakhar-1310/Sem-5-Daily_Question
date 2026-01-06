ca6d8e2cfd49e9c3cc0e14ef17c6f21f17081467
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-01-06 13:09:03 +0530

    Time: 27 ms (18.7%), Space: 54.3 MB (60.71%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,31 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        return coinBU(coins,amount);
+        int dp[][] = new int[amount+1][coins.length];
+        for(int i[] : dp){
+            Arrays.fill(i,-1);
+        }
+        return helper(amount,coins,0,dp);
     }
-    public static int coinBU(int coin[], int amount) {
-		int dp[][] = new int[coin.length+1][amount+1];
-		for(int i=0;i<dp.length;i++) {
-			dp[i][0]=1;
-		}
-		for(int i=1;i<dp.length;i++) {
-			for(int am = 1;am<dp[0].length;am++) {
-				int inc =0;
-				int exc =0;
-				if(am>=coin[i-1]) {
-					inc = dp[i][am-coin[i-1]]; 
-				}
-				exc = dp[i-1][am];
-				dp[i][am]= inc + exc;
-			}
-		}
-		return dp[dp.length-1][dp[0].length-1];
-	}
+
+    public int helper(int amount, int coins[], int i, int dp[][]){
+        if(amount==0){
+            return 1;
+        }
+        if(i>=coins.length){
+            return 0;
+        }
+        if(dp[amount][i]!=-1){
+            return dp[amount][i];
+        }
+
+        int inc=0;
+        if(coins[i]<=amount){
+            inc = helper(amount-coins[i],coins,i,dp);
+        }
+
+        int exc = helper(amount,coins,i+1,dp);
+
+        return dp[amount][i]=inc+exc;
+    }
+    
 }
