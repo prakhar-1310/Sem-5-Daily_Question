6e124b880b7646601f233375eb0ec39c1dd11bd5
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-01-11 12:26:04 +0530

    Time: 6 ms (58.64%), Space: 45 MB (48.81%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -1,15 +1,15 @@
 class Solution {
-    private int MOD = 1_000_000_007 ;
     public int countHousePlacements(int n) {
-        int[] dp = new int[n+1] ;
-        Arrays.fill(dp,-1) ;
-        fibo(n,dp) ;
-        return (int)((1L * dp[n] * dp[n]) % MOD);
-    }
-    public int fibo(int n , int[] dp){
-        if(n==1)return dp[1] = 2 ;
-        if(n==2)return dp[2] = 3 ;
-        if(dp[n]!=-1)return dp[n] ;
-        return dp[n] = (fibo(n-1,dp)+fibo(n-2,dp))%MOD ;
+        int mod = 1000000007;
+        int dp[] = new int[n+2];
+        dp[0]=1;
+        dp[1]=2;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%mod;
+        }
+
+        return (int)((1L*dp[n]*dp[n])%mod);
+        
     }
 }
