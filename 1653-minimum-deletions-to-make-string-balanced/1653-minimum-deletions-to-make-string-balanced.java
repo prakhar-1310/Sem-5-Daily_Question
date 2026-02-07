1b5f565c505500f0ad4dad3d48f07cac6c8e1f4d
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-02-07 18:52:45 +0530

    Time: 21 ms (68.91%), Space: 47.5 MB (87.99%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minimumDeletions(String s) {
-        Stack<Character> st = new Stack<>();
-        int count=0;
-        for(int i=0;i<s.length();i++){ //all b after all a
-            if(!st.isEmpty() && st.peek()=='b' && s.charAt(i)=='a'){
-                st.pop();
-                count++;
+        int del =0;
+        int countB=0;
+        for(char c : s.toCharArray()){
+            if(c=='a'){
+                del = Math.min(del+1, countB); // del+1 is for deleting 'a'
+                                                // countB for deleting 'b'
             }
             else{
-                st.push(s.charAt(i));
+                countB++;
             }
         }
-        return count;
+
+        return del;
     }
 }
