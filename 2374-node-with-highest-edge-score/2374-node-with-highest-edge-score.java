ca987ef9e90694ab633df6ad4ccc1450d3b3f604
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-02-24 10:25:42 +0530

    Time: 143 ms (6.47%), Space: 212.6 MB (5%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -1,27 +1,27 @@
 class Solution {
     public int edgeScore(int[] edges) {
         HashMap<Integer,List<Integer>>map = new HashMap<>();
-        for(int i =0;i<edges.length;i++){
+        for(int i=0;i<edges.length;i++){
             int v1 = i;
             int v2 = edges[i];
-            if(!map.containsKey(v2)){
-                map.put(v2, new ArrayList<>());
+            if(!map.containsKey(v1)){
+                map.put(v1,new ArrayList<>());
             }
-            map.get(v2).add(v1);
+            map.get(v1).add(v2);
         }
 
-        long max=Integer.MIN_VALUE;
-        int node =0;
-        for(int i : map.keySet()){
-            long sum=0;
-            for(int j : map.get(i)){
-                sum+=j;
+        long in[] = new long[edges.length];
+
+        for(int v : map.keySet()){
+            for(int i : map.get(v)){
+                in[i]+=v;
             }
-            if(sum>max){
-                max = sum;
-                node = i;
-            }
-            else if(sum==max && node>i){
+        }
+        long max=0;
+        int node=0;
+        for(int i=0;i<in.length;i++){
+            if(max<in[i]){
+                max=in[i];
                 node=i;
             }
         }
