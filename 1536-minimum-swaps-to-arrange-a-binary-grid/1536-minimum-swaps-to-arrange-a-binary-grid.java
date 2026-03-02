907e921502ca512cb4c3d22bbf1ff6b8e7d72da0
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-03-02 23:36:56 +0530

    Time: 2 ms (96.23%), Space: 49.8 MB (47.17%) - LeetHub

---
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minSwaps(int[][] grid) {
+        int n = grid.length;
+        int zeros[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=n-1;j>=0 && grid[i][j]==0; j--){
+                cnt++;
+            }
+            zeros[i]=cnt;
+        }
+
+        int swaps=0;
+
+        for(int i=0;i<n;i++){
+            int needed = n-i-1;
+            int j=i;
+            while(j<n && zeros[j]<needed){
+                j++;
+            }
+            
+            if(j==n)return -1;
+
+            while(i<j){
+                int temp = zeros[j];
+                zeros[j]=zeros[j-1];
+                zeros[j-1]=temp;
+                swaps++;
+                j--;
+            }
+            
+        }
+
+        return swaps;
+    }
+}
