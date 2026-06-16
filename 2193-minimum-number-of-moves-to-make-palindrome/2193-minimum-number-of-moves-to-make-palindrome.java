d504f35951c083d2225fce56c36c0de9a38d615c
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-06-16 22:00:05 +0530

    Time: 11 ms (73.8%), Space: 43.4 MB (60.96%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.java
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.java
@@ -1,42 +1,39 @@
 class Solution {
     public int minMovesToMakePalindrome(String s) {
-        int cnt=0;
-        while(s.length()>2){
-            char st = s.charAt(0);
-            char ed = s.charAt(s.length()-1);
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int ans = 0;
 
-            if(st==ed){
-                s = s.substring(1,s.length()-1);
+        int i = 0, j = n - 1;
+
+        while (i < j) {
+
+            int k = j;
+
+            while (k > i && arr[k] != arr[i]) {
+                k--;
             }
 
-            else{
-                int id1 = s.lastIndexOf(st);
-                int id2 = s.indexOf(ed);
-
-                int step1 = s.length()-1-id1;//replace id1 index character with last
-                int step2 = id2;// replace first character with id2 index character
-                int len = s.length();
-                StringBuilder sb = new StringBuilder();
-                if (step1 > step2) {
-                    cnt += step2;
-                    sb.append(s.substring(0, id2));
-                    sb.append(s.substring(id2 + 1, len - 1));
-                } else {
-                    cnt += step1;
-                    sb.append(s.substring(1, id1));
-                    sb.append(s.substring(id1 + 1));
+            if (k == i) {
+                swap(arr, i, i + 1);
+                ans++;
+            } else {
+                while (k < j) {
+                    swap(arr, k, k + 1);
+                    ans++;
+                    k++;
                 }
-
-                s = sb.toString();
+                i++;
+                j--;
             }
         }
 
-        return cnt;
+        return ans;
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
-// UML diagram
-// relation, association, agrregation
-// is a, has 
-// elevator system
-// class diagram-> methods, public +, private -, protected #
-// singleton design pattern
