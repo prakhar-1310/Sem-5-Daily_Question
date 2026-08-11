237327db57013093e35f0096710e7bc7bba229b7
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-08-12 01:28:05 +0530

    Time: 0 ms (100%), Space: 41.8 MB (90.34%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,24 +1,22 @@
 class Solution {
     public int integerReplacement(int n) {
-        HashMap<Long,Long>map = new HashMap<>();
-        return (int)helper(n, map);
-    }
+        int cnt=0;
+        long num = n;
+        while(num!=1){
+            if(num%2==0){
+                num/=2;
+            }
+            else{
+                if(num==3 || (num&2)==0){
+                    num--;
+                }
+                else{
+                    num++;
+                }
+            }
+            cnt++;
+        }
 
-    public long helper(long n, HashMap<Long,Long>map){
-        if(n==1){
-            return 0;
-        }
-        if(map.containsKey(n))return map.get(n);
-        long ans=0;
-        if(n%2==0){
-            ans = helper(n/2,map);
-        }
-        else{
-            long opt1 = helper(n+1,map);
-            long opt2 = helper(n-1,map);
-            ans = Math.min(opt1, opt2);
-        }
-        map.put(n, ans+1);
-        return ans+1;
+        return cnt;
     }
 }
