15f0a59a2f00cf2dc29cd901fe07e96b142e7e55
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-08-25 13:45:14 +0530

    Time: 2 ms (98.51%), Space: 44.2 MB (76.7%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,17 +1,57 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        List<int[]>list = new ArrayList<>();
+        double low = 0;
+        double high = 1.0;
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                list.add(new int[]{arr[i], arr[j]});
+        while(low<=high){
+            double mid = low + (high-low)/2;
+
+            Pair p = helper(arr, mid);
+
+            if(p.cnt == k){
+                return new int[]{p.num, p.deno};
+            }
+
+            if(p.cnt>k){
+                high=mid;
+            }
+            else{
+                low = mid;
             }
         }
 
-        Collections.sort(list, (a,b)->{
-            return Double.compare(1.0*a[0]/a[1] , 1.0*b[0]/b[1]);
-        });
+        return null;
+    }
 
-        return list.get(k-1);
+    public Pair helper(int arr[], double value){
+        int num=arr[0];
+        int den=arr[arr.length-1];
+        int i=0;
+        int cnt=0;
+        for(int j=1;j<arr.length; j++){
+            while(1.0*arr[i]/arr[j]<=value){
+                i++;
+            }
+            cnt+=i;
+
+            if(i>0 && 1.0*arr[i-1]/arr[j] > 1.0*num/den){
+                num=arr[i-1];
+                den=arr[j];
+            }
+        }
+
+        return new Pair(cnt, num, den);
+    }
+
+    class Pair{
+        int cnt;
+        int num;
+        int deno;
+
+        public Pair(int cnt, int n, int d){
+            this.cnt=cnt;
+            this.num=n;
+            this.deno=d;
+        }
     }
 }
