e87a0b036840a855a6f0b1b2fbd917fcf7f36a1f
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-08-27 13:09:41 +0530

    Time: 2 ms (41.89%), Space: 44.4 MB (20.27%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,37 +1,35 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        String ans = "";
-        int n = s.length();
+        List<String>list = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        int left=0;
+        int ans=Integer.MAX_VALUE;
+        int cnt=0;
+        for(int right=0;right<s.length();right++){
+            if(s.charAt(right)=='1'){
+                cnt++;
+            }
 
-            int oneCnt = 0;
-            StringBuilder cur = new StringBuilder();
-
-            for (int j = i; j < n; j++) {
-
-                cur.append(s.charAt(j));
-
-                if (s.charAt(j) == '1')
-                    oneCnt++;
-
-                // More than k ones can never become valid again
-                if (oneCnt > k)
-                    break;
-
-                if (oneCnt == k) {
-                    String curStr = cur.toString();
-
-                    if (ans.isEmpty() ||
-                        curStr.length() < ans.length() ||
-                        (curStr.length() == ans.length() && curStr.compareTo(ans) < 0)) {
-
-                        ans = curStr;
-                    }
+            while(cnt==k){
+                if(ans==(right-left+1)){
+                    list.add(s.substring(left,right+1));
                 }
+                else if(ans>(right-left+1)){
+                    ans=right-left+1;
+                    list=new ArrayList<>();
+                    list.add(s.substring(left,right+1));
+                }
+
+                if(s.charAt(left)=='1'){
+                    cnt--;
+                }
+
+                left++;
             }
         }
 
-        return ans;
+        Collections.sort(list);
+        if(list.size()==0)return "";
+        return list.get(0);
     }
 }
