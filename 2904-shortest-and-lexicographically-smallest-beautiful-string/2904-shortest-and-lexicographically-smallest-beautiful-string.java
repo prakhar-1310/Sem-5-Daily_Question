ebc50ecc3883256f2f6c777eb9374d1c8804d85e
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-08-27 13:30:49 +0530

    Time: 1 ms (100%), Space: 43.9 MB (75.68%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,23 +1,24 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        List<String>list = new ArrayList<>();
-
+        String ans="";
         int left=0;
-        int ans=Integer.MAX_VALUE;
+        int len=Integer.MAX_VALUE;
         int cnt=0;
         for(int right=0;right<s.length();right++){
             if(s.charAt(right)=='1'){
                 cnt++;
             }
-
             while(cnt==k){
-                if(ans==(right-left+1)){
-                    list.add(s.substring(left,right+1));
+                if(ans==""){
+                    ans=s.substring(left, right+1);
                 }
-                else if(ans>(right-left+1)){
-                    ans=right-left+1;
-                    list=new ArrayList<>();
-                    list.add(s.substring(left,right+1));
+                if(len==(right-left+1) && ans.compareTo(s.substring(left, right+1))>0){
+                    ans=s.substring(left,right+1);
+                }
+                else if(len>(right-left+1)){
+                    len=right-left+1;
+                    
+                    ans = (s.substring(left,right+1));
                 }
 
                 if(s.charAt(left)=='1'){
@@ -28,8 +29,6 @@ class Solution {
             }
         }
 
-        Collections.sort(list);
-        if(list.size()==0)return "";
-        return list.get(0);
+        return ans;
     }
 }
