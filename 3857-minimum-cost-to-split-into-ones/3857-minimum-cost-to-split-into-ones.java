aeabb88252c11f9b4be002d1048d6625ca491e5f
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-06 17:49:58 +0530

    Time: 0 ms (100%), Space: 42.6 MB (24.75%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
@@ -1,9 +1,6 @@
 class Solution {
     public int minCost(int n) {
-        int sum=0;
-        for(int i=2;i<=n;i++){
-            sum+=i-1;
-        }
-        return sum;
+        n--;
+        return n*(n+1)/2;
     }
 }
