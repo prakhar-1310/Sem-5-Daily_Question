259f9977dfb2264fa76e8c1e35bc8bae9d9bb4a1
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-06 19:48:57 +0530

    Time: 59 ms (51.91%), Space: 105.7 MB (45.11%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -4,18 +4,12 @@ class Solution {
         for(int i : nums){
             set.add(i);
             int n=0;
-            int flag=0;
             while(i>0){
                 int rem = i%10;
                 i/=10;
-                if(flag==0 && rem==0){
-                    continue;
-                }
-                else{
-                    flag=1;
-                    n*=10;
-                    n+=rem;
-                }
+                n*=10;
+                n+=rem;
+                
             }
             set.add(n);
         }
