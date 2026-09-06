8b3cbbddc7cab10347513ba9e55ae821a6d4f57f
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-06 12:38:18 +0530

    Time: 21 ms (33.78%), Space: 54.6 MB (27.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,23 +1,34 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        return coinBU(s,t);
+        int dp[][] = new int[s.length()][t.length()];
+        for(int i[] : dp){
+            Arrays.fill(i,-1);
+        }
+        return helper(0, 0, s, t, dp);
     }
-    public static int coinBU(String s, String t) {
-		int dp[][] = new int[s.length()+1][t.length()+1];
-		for(int i=0;i<dp.length;i++) {
-			dp[i][0]=1;
-		}
-		for(int i=1;i<dp.length;i++) {
-			for(int j = 1;j<dp[0].length;j++) {
-				int inc =0;
-				int exc =0;
-				if(s.charAt(i-1)==t.charAt(j-1)) {
-					inc = dp[i-1][j-1];
-				}
-				exc = dp[i-1][j];
-				dp[i][j]= inc + exc;
-			}
-		}
-		return dp[dp.length-1][dp[0].length-1];
-	}
+
+    public int helper(int i, int j, String s, String t, int dp[][]){
+        if(j==t.length()){
+            return 1;
+        }
+
+        if(i==s.length()){
+            return 0;
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int ans=0;
+
+        if(s.charAt(i)==t.charAt(j)){
+            ans+=helper(i+1, j+1, s, t, dp);
+        }
+
+        ans+=helper(i+1,j,s,t, dp);
+
+        return dp[i][j]=ans;
+    }
+
 }
