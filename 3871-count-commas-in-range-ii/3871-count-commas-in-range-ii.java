e3870261e38f66cb7f050ee9d1930e178da1b818
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-09 11:50:49 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (56.41%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -5,7 +5,6 @@ class Solution {
         long tot=0;
         long st = 1000;
         while(st<=n){
-            int len = (st+"").length()-1;
             tot+=n-(st-1);
             st*=1000;
         }
