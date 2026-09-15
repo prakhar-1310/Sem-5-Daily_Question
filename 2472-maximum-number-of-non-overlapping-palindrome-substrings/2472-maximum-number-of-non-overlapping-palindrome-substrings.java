b01439c6b22b05cdf50cbc8deda48245f0dd03c9
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-15 11:55:15 +0530

    Time: 115 ms (32.16%), Space: 57.8 MB (45.73%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,41 +1,39 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
-        if(k==1)return s.length();
-        int dp[] = new int[s.length()];
+        int n = s.length();
+        if(k==1)return n;
+        int dp[] = new int[n];
         Arrays.fill(dp, -1);
-        return helper(0,s,k, dp);
+
+        boolean palin[][] = new boolean[n][n];
+        for(int i=n-1; i>=0; i--){
+            for(int j=i;j<n;j++){
+                if(s.charAt(i)==s.charAt(j) && (j-i<=2 || palin[i+1][j-1])){
+                    palin[i][j]=true;
+                }
+            }
+        }
+        return helper(0,s,k, dp, palin);
     }
 
-    public int helper(int idx, String s, int k, int dp[]){
+    public int helper(int idx, String s, int k, int dp[], boolean palin[][]){
         if(idx>=s.length()){
             return 0;
         }
         if(dp[idx]!=-1)return dp[idx];
         int ans=0;
 
-        ans = helper(idx+1, s, k, dp); //skip
+        ans = helper(idx+1, s, k, dp, palin); //skip
         
         for(int i=idx+k-1;i<s.length();i++){
             int opt=0;
-            if(check(s, idx, i)){
-                opt=1+helper(i+1, s, k, dp);
+            if(palin[idx][i]){
+                opt=1+helper(i+1, s, k, dp, palin);
             }
 
             ans = Math.max(ans, opt);
         }
 
         return dp[idx]=ans;
-    }
-
-    public boolean check(String s, int i, int j){
-        while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-
-        return true;
     }
 }
