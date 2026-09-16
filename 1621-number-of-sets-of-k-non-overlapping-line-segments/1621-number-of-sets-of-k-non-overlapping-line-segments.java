efe4c5f7c36116a9cf4f2a8daccb5025954f7e4f
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-16 13:53:37 +0530

    Time: 173 ms (42.58%), Space: 162.1 MB (38.06%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -15,15 +15,15 @@ class Solution {
     public long helper(int i, int k, int curr, int n, long dp[][][], int start){
         if(k==curr) return 1;
 
-        if(i>=n)return 0;
+        if(i>n)return 0;
 
         if(dp[i][curr][start]!=-1)return dp[i][curr][start];
         long ans=0;
         if(start==0){ // means need to start
-            ans+= helper(i, k, curr, n, dp, 1)%mod;
+            ans+= helper(i+1, k, curr, n, dp, 1)%mod;
         }
         else{ // means need to end
-            ans+= helper(i+1, k, curr+1, n, dp, 0)%mod;
+            ans+= helper(i, k, curr+1, n, dp, 0)%mod;
         }
 
         ans = (ans + helper(i+1, k, curr, n, dp, start))%mod; // skip
