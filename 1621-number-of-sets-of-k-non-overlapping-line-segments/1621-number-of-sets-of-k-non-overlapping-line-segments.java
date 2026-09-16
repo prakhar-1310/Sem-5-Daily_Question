db14272ec5c776ad6f75becfd45b93b3e8d8744b
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-16 13:44:13 +0530

    Time: 164 ms (44.52%), Space: 161.9 MB (38.71%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,45 +1,33 @@
 class Solution {
     long mod = 1000000007;
-    long suff[][];
     public int numberOfSets(int n, int k) {
-        long dp[][] = new long[n+1][k+1];
-        suff = new long[n+1][k+1];
-        for(long i[] : suff){
-            Arrays.fill(i, -1);
+        long dp[][][] = new long[n+1][k+1][2];
+
+        
+        for(long i[][] : dp){
+            for(long j[] : i){
+                Arrays.fill(j, -1);
+            }
         }
-        for(long i[] : dp){
-            Arrays.fill(i, -1);
-        }
-        return (int)((helper(1, k, 0, n, dp)+mod)%mod);
+        return (int)((helper(1, k, 0, n, dp, 0)+mod)%mod);
     }
 
-    public long helper(int i, int k, int curr, int n, long dp[][]){
-        if(curr==k){
-            return 1;
-        }
+    public long helper(int i, int k, int curr, int n, long dp[][][], int start){
+        if(k==curr) return 1;
 
         if(i>=n)return 0;
 
-        if(dp[i][curr]!=-1){
-            return dp[i][curr];
+        if(dp[i][curr][start]!=-1)return dp[i][curr][start];
+        long ans=0;
+        if(start==0){ // means need to start
+            ans+= helper(i, k, curr, n, dp, 1)%mod;
+        }
+        else{ // means need to end
+            ans+= helper(i+1, k, curr+1, n, dp, 0)%mod;
         }
 
-        long ans = suffix(i+1, k, curr+1, n, dp)%mod;
-        long temp = helper(i+1, k, curr, n, dp)%mod;
-        ans = (ans+temp)%mod;
-        return dp[i][curr]=ans;
+        ans = (ans + helper(i+1, k, curr, n, dp, start))%mod; // skip
+
+        return dp[i][curr][start]=ans;
     }
-
-    public long suffix(int i, int k, int curr, int n, long dp[][]){
-        if(i>n)return 0;
-
-        if(suff[i][curr]!=-1)return suff[i][curr];
-
-        long ans=helper(i, k, curr, n, dp);
-
-        ans = (ans+suffix(i+1, k, curr, n, dp))%mod;
-
-        return suff[i][curr]=ans;
-    }
-
 }
