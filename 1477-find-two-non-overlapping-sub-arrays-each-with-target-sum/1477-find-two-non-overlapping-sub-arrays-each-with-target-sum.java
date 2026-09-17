302a2696a5b7e36247eb6e405981b9407f219cb9
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-17 17:41:28 +0530

    Time: 14 ms (40.36%), Space: 91.6 MB (43.95%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,48 +1,59 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
         int n = arr.length;
-        int end[] = new int[n];
-        Arrays.fill(end, -1);
+        int pref[] = pre(arr, target);
+        int suff[] = suf(arr, target);
+        int ans = Integer.MAX_VALUE;
 
+        for(int i=0;i<n-1;i++){
+            if(suff[i+1]!= Integer.MAX_VALUE && pref[i]!=Integer.MAX_VALUE){
+                ans = Math.min(ans,pref[i]+suff[i+1]);
+            }
+        }
+
+        return ans==Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public int[] pre(int arr[], int tar){
+        int n = arr.length;
+        int ans[] = new int[n];
+        int min = Integer.MAX_VALUE;
         int left=0;
         int sum=0;
         for(int right=0;right<n;right++){
             sum+=arr[right];
 
-            while(sum>=target){
-                if(sum==target){
-                    end[left]=right;
+            while(sum>=tar){
+                if(sum==tar){
+                    min = Math.min(right-left+1,min);
                 }
                 sum-=arr[left++];
             }
+            ans[right]=min;
+
         }
 
-        int dp[][] = new int[n][3];
-        for(int i[] : dp){
-            Arrays.fill(i, -1);
-        }
-
-        int ans = helper(0, end, 0, dp);
-        return ans==Integer.MAX_VALUE/2 ? -1 : ans;
-
+        return ans;
     }
 
-    public int helper(int idx, int end[], int cnt, int dp[][]){
-        if(cnt==2)return 0;
+    public int[] suf(int arr[], int tar){
+        int n = arr.length;
+        int ans[] = new int[n];
+        int min = Integer.MAX_VALUE;
+        int right=n-1;
+        int sum=0;
+        for(int left=n-1;left>=0;left--){
+            sum+=arr[left];
 
-        if(idx>=end.length)return Integer.MAX_VALUE/2;
-        if(dp[idx][cnt]!=-1)return dp[idx][cnt];
+            while(sum>=tar){
+                if(sum==tar){
+                    min=Math.min(right-left+1,min);
+                }
+                sum-=arr[right--];
+            }
+            ans[left]=min;
+        }
 
-        // skip
-        int opt1 = helper(idx+1, end, cnt, dp);
-
-        // take(end here)
-        if(end[idx]==-1)return dp[idx][cnt]=opt1;
-        int fir = end[idx]-idx+1;
-        int sec = helper(end[idx]+1, end, cnt+1, dp);
-        
-        if(sec==Integer.MAX_VALUE/2)return dp[idx][cnt]=opt1;;
-
-        return dp[idx][cnt] = Math.min(opt1, fir+sec);
+        return ans;
     }
 }
