7d2dc892de0c73fb25946c0c93579a98569e22ac
Prakhar Shahi <164357278+prakhar-1310@users.noreply.github.com>
2026-09-23 01:14:07 +0530

    Time: 550 ms (54.32%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,20 @@
+SELECT product_id, new_price AS price
+FROM (
+    SELECT
+        product_id,
+        new_price,
+        ROW_NUMBER() OVER (
+            PARTITION BY product_id
+            ORDER BY change_date DESC
+        ) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+) t
+WHERE rn = 1
+
+union 
+
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
